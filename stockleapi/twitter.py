e6fe3d0e1c15bf5f4161c3fde015e515a8efabe8
Valerio Basile <valeriobasile@gmail.com>
2016-09-14 18:28:15 +0200

    fixed twitter search

--- stockleapi/twitter.py
+++ stockleapi/twitter.py
@@ -3,30 +3,31 @@ from requests_oauthlib import OAuth1
 import json
 import logging as log
 from os.path import join, dirname
-
-# read the configuration file
-with open(join(dirname(__file__),'config/config.yaml')) as fd_conf:
-    config = yaml.load(fd_conf)
+import config
 
 # OAuth 1 authentication
-auth = OAuth1(config['consumer_key'],
-              config['consumer_secret'],
-              config['oauth_token'],
-              config['oauth_secret'])
+auth = OAuth1(config.consumer_key,
+              config.consumer_secret,
+              config.oauth_token,
+              config.oauth_secret)
 
 def get_keywords_stream(keyword):
     # POST data: list of keywords to search
-    data = {'track':keyword}
-    response = requests.post(config['url_filter'], data=data, auth=auth, stream=True)
+    data = {'language':'en', 'track':keyword}
+    response = requests.post(config.url_filter, data=data, auth=auth, stream=True)
 
     tweets = []
     for line in response.iter_lines():
         if line:
             try:
                 tweet = json.loads(line)
-                tweets.append(json.dumps(tweet))
+                tweets.append(tweet)
             except:
                 log.error('error parsing tweet')
-        if len(tweets) >= config['response_size']:
+        if len(tweets) >= config.tweet_list_size:
             break
     return tweets
+
+def get_tweets(keyword):
+    tweets = get_keywords_stream(keyword)
+    return [tweet['text'] for tweet in tweets]
