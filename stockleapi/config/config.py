2276fb5e2f1a860108e3bf439586fc016ca7b093
Valerio Basile <valeriobasile@gmail.com>
2016-09-14 17:33:05 +0200

    configuration file is now a Python file

---
+++ stockleapi/config/config.py
@@ -0,0 +1,11 @@
+# OAuth credential for the Twitter API
+consumer_key = "PUT_YOUR_AUTHENTICATION_INFO_HERE"
+consumer_secret = "PUT_YOUR_AUTHENTICATION_INFO_HERE"
+oauth_token = "PUT_YOUR_AUTHENTICATION_INFO_HERE"
+oauth_secret = "PUT_YOUR_AUTHENTICATION_INFO_HERE"
+
+# URL of the Twitter API for the keyword search stream
+url_filter = https://stream.twitter.com/1.1/statuses/filter.json
+
+# how may tweets are returned
+tweet_list_size = 10
