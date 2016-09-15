5d19758d467819ba39da002044bb7aa7795fda00
Valerio Basile <valeriobasile@gmail.com>
2016-09-15 19:26:56 +0200

    better mock data

--- stockleapi/twitter.py
+++ stockleapi/twitter.py
@@ -54,17 +54,19 @@ def get_items(keyword):
         except:
             pass
     return items
+
 '''
 
 def get_items(query):
-    items = {'items':[]}
-
-    for i in range(config.tweet_list_size):
-        item = {}
-        item["id"] = "TWEETID" + str(i)
-        item["text"] = "Tweet about " + query + " " + str(i)
-        item["sentiment"] = random.random() * 2 - 1
-
-        items['items'].append(item)
+    return {'items':[
+             {'text': u"my headphone jack isn't working anymore what kind of apple sorcery is this",
+              'id': '776471755049226240',
+              'sentiment': -0.575447},
+             {'text': u'Apple shares jump 2% again ahead of iPhone 7 sales https://t.co/u8OFrrMDOs #Tips2Trade #T2T https://t.co/L7mblr95FV',
+              'id': '776471754399068160',
+              'sentiment': 0.0},
+             {'text': u'Neoliberalism: \u2018Profit Goes To Apple And Microsoft, Not To The Taxpayer\u2019 \u2013 By Kit\xa0O\u2019Connell https://t.co/iaZ0adCw9I https://t.co/hetR4jb0QC',
+              'id': '776471749948944384',
+              'sentiment': -0.321275}]}
 
     return items
