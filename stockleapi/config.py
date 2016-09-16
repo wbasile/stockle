26d329c008869b9b85098977cd95745ea307069f
Valerio Basile <valeriobasile@gmail.com>
2016-09-16 22:39:22 +0200

    added News API wrapper

--- stockleapi/config.py
+++ stockleapi/config.py
@@ -7,8 +7,14 @@ oauth_secret = ""
 # URL of the Twitter API for the keyword search stream
 url_twitter = 'https://api.twitter.com/1.1/search/tweets.json'
 
+# base URL of the AlchemyData News API
+url_news = 'http://gateway-a.watsonplatform.net/calls/data/GetNews'
+
 # how may tweets are returned
 tweet_list_size = 50
+
+# how may news items are returned
+news_list_size = 50
 
 # key for Alchemy API
 alchemyapi_key = ""
--- stockleapi/news.py
+++ stockleapi/news.py
@@ -1,14 +1,53 @@
 import random
+import requests
+import config
+from alchemy import AlchemyAPI
 
-def get_items(query,n=10):
+# Create the AlchemyAPI Object
+alchemy = AlchemyAPI()
+
+'''
+def get_items(query):
+    data = {'apikey':config.alchemyapi_key ,
+            'outputMode':'json',
+            'start':'now-1d',
+            'end':'now',
+            'count':config.news_list_size,
+            'q.enriched.url.title':query,
+            'return':'original.url,enriched.url.title'}
+    response = requests.get(config.url_news, params=data)
+    results = response.json()
     items = {'items':[]}
-
-    for i in range(n):
-        item = {}
-        item["title"] = "news article about " + query + " " + str(i)
-        item["url"] = "http://www.blahblah.com/news_article_" + query + "_" + str(i)
-        item["sentiment"] = random.random() * 2 - 1
-
-        items['items'].append(item)
-
+    for item in results['result']['docs']:
+        response_sentiment = alchemy.sentiment_targeted('url', item['source']['original']['url'], query)
+        try:
+            sentiment = response_sentiment['docSentiment']['type']
+            if sentiment == 'positive' or sentiment == 'negative':
+                sentiment_score = eval(response_sentiment['docSentiment']['score'])
+            else:
+                sentiment_score = 0.0
+            items['items'].append({'title' : item['source']['enriched']['url']['title'],
+                                   'url' : item['source']['original']['url'],
+                                   'sentiment' : sentiment_score})
+        except:
+            pass
     return items
+'''
+
+def get_items(query):
+    return {'items': [
+             {'url': u'http://finance.yahoo.com/m/274e68d5-b53b-3a13-b5fe-0686b4f501f4/apple-end-four-day-winning.html?guid=096BAA7E-7C17-11E6-AA97-CF05589A95D8&siteid=yhoof2&yptr=yahoo',
+             'sentiment': -0.412696,
+             'title': u'Apple end four-day winning streak'},
+            {'url': u'https://itunes.apple.com/us/post/idsa.09ce2c03-a1b4-11e5-8838-cb4822dc8c3d',
+             'sentiment': 0.299896,
+             'title': u'Watch \u201cThe 1989 World Tour \u2014 Live (Preview)\u201d posted by Taylor Swift on Apple Music.'},
+            {'url': u'http://uk.reuters.com/article/uk-apple-japan-tax-idUKKCN11M09M?il=0',
+             'sentiment': -0.571383,
+             'title': u'Apple Japan unit ordered to pay $118 million tax for underreporting income - media'},
+            {'url': u'http://finance.yahoo.com/news/apple-lifts-tech-etfs-during-140000778.html',
+             'sentiment': -0.168371,
+             'title': u'Apple Lifts Tech ETFs During Market Slump'},
+            {'url': u'https://ca.news.yahoo.com/apple-stock-heads-best-four-days-since-2014-170818906--finance.html',
+             'sentiment': -0.446473,
+             'title': u'Apple stock heads for best four days since 2014; short sellers flee'}]}
